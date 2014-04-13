27911ea64e71a40e4bdb0659e0ed7385b3b4a47e
Sergio CORTES (From Sparrow) <sacortesh@github.com>
2014-04-13 19:38:14 +0200

    Agregado ActiviteDAO, ControleurCentre y firmas de metodos necesarios para satisfacer el caso de uso "AjouterActivite"

---
+++ src/main/java/com/unsoft/acl_grenoble/model/dao/ActiviteDAO.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.unsoft.acl_grenoble.model.dao;
+
+import com.unsoft.acl_grenoble.model.centre.Competence;
+import com.unsoft.acl_grenoble.model.centre.Periode;
+import com.unsoft.acl_grenoble.model.centre.Theme;
+import java.util.Date;
+import java.util.List;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author sparrow
+ */
+public class ActiviteDAO extends AbstractDataBaseDAO{
+
+    public ActiviteDAO(DataSource dataSource) {
+        super(dataSource);
+    }
+    
+    public List<Theme> getThemesPossibles() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public List<Periode> getPeriodesPossibles() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public int createActivite(String nom, String descriptif, int nmMaxAnim, 
+            String nomCentre, String nomTheme){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public boolean lierCompetences(int idActivite, List<Competence> competences){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public boolean creerPeriode(Date dateDebut, Date dateFin){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public boolean lierEtat(int idActivite, String periode, String etat){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
