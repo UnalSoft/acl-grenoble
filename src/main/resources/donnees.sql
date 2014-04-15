c8128265e4cfdc0e7716b9916019c742a50bf0a9
martijua <martijua@ensipc23.imag.fr>
2014-04-15 15:53:06 +0200

    Filling DB

---
+++ src/main/resources/donnees.sql
@@ -0,0 +1,5 @@
+/*insert into compte values ('Richard.Fuller','1234',1);
+insert into utilisateur values ('Fuller','Richard','Richard.Fuller','richard.fuller@mail.com');*/
+
+/*select * from compte;*/
+select * from UTILISATEUR;
